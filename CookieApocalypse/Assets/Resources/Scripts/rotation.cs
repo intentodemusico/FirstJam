627cb21524ae78242918463d5e67ecf86a511957
Oswalman <oswaldo-007@hotmail.com>
2019-06-17 18:41:59 -0500

    plataforma

---
+++ CookieApocalypse/Assets/Resources/Scripts/rotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(Vector3.forward * -1);
+        //suelo.transform.rotation = new Quaternion(0f, 0, change, 0);
+        //change += 20;
+    }
+}
