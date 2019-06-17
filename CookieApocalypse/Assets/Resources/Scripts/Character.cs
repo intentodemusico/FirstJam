5cbf7cf0feaf55c3fd6d176ecf9185f5732c4bb7
Oswalman <oswaldo-007@hotmail.com>
2019-06-17 16:30:08 -0500

    movimiento personaje, sonido, algunos obstaculos

---
+++ CookieApocalypse/Assets/Resources/Scripts/Character.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character : MonoBehaviour
+{
+    [Header("Velocidad de movimiento:")]
+    public float speed;
+    [Header("Fuerza de salto: ")]
+    public float jump;
+   
+   
+    private Rigidbody2D rb;
+    public AudioClip Sound;
+    private Animator anim;
+    public float distGround;
+    private AudioSource source;
+    private bool isOnGround;
+    //Reloj
+    bool timerReached;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        rb = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+        timer = 0;
+        anim = GetComponent<Animator>();
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo") )
+        {
+            isOnGround = true;
+        }
+    }
+    private IEnumerator waiting()
+    {
+        Sound = (AudioClip)Resources.Load("Audio/morir", typeof(AudioClip));
+        source.PlayOneShot(Sound, 2f);
+        yield return new WaitWhile(()=>source.isPlaying);
+        Scene escena = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(escena.name);
+        
+        // Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hider") )
+        {
+            var obj = GameObject.FindWithTag("Texto");
+            if (obj != null)
+            {
+                obj.SetActive(false);
+            }
+
+        }
+        else if (collision.CompareTag("KillZone"))
+        {
+            StartCoroutine(waiting());
+
+        }
+    }
+
+  
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            isOnGround = false;
+        }
+    }
+    private void Update()
+    {
+        anim.SetFloat("Speed",Mathf.Abs( rb.velocity.x));
+        anim.SetBool("Grounded", isOnGround);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movimiento = new Vector2(movHorizontal, 0f);
+        rb.AddForce(movimiento * speed * 2f);
+       // rb.rotation -= movHorizontal*speed;
+        if (Input.GetButtonDown("Jump") && isOnGround) //Devuelve verdadero en  el frame que se oprimió
+        {
+            Sound = (AudioClip)Resources.Load("Audio/jump", typeof(AudioClip));
+            source.PlayOneShot(Sound, 2f);
+           
+            rb.AddForce(Vector2.up*jump*50f,ForceMode2D.Force);
+        }
+        /*
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb.AddForce(Vector2.right*speed);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb.AddForce(Vector2.left * speed);
+        }
+        else if(Input.GetAxis("Vertical") > 0)
+
+        {
+            rb.AddForce(Vector2.up * speed*5.0f);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+
+        {
+            rb.AddForce(Vector2.down * speed * 5.0f);
+        }*/
+
+    }
+}
---
+++ CookieApocalypse/Assets/Resources/Scripts/camaraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraFollow : MonoBehaviour
+{
+    [Header("Objeto a seguir con la camara: ")]
+    public GameObject followObject;
+    private Vector3 camPosition;
+    public float offsetX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        camPosition = new Vector3(followObject.transform.position.x + offsetX, followObject.transform.position.y, -20f);
+        transform.position = camPosition;
+    }
+}
