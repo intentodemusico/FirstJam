f8c67c28d7d904df59b82b49180f71118359339d
? <alberto.garcia.2016@upb.edu.co>
2019-06-18 00:27:47 -0500

    asdfgnhfdsa

--- CookieApocalypse/Assets/Resources/Scripts/enemySpawner.cs
+++ CookieApocalypse/Assets/Resources/Scripts/enemySpawner.cs
@@ -9,13 +9,15 @@ public class enemySpawner : MonoBehaviour
     private bool canSpawn = true;
     
     
-    [Header("Spawn limit")] [Tooltip("Se debe ingresar la cantidad máxima de enemigos a spawnear.")] 
-    public int spawnLimit=30;
+    //[Header("Spawn limit")] [Tooltip("Se debe ingresar la cantidad máxima de enemigos a spawnear.")] 
+    private int spawnLimit=50;
     
     private Vector2 whereToSpawn;
-    [Header("Spawn rate")] [Tooltip("Se debe ingresar la velocidad de spawneo de los enemigos.")] public float spawnRate = 10f;
+    // [Header("Spawn rate")] [Tooltip("Se debe ingresar la velocidad de spawneo de los enemigos.")] 
+    private float spawnRate = 8f;
     
-    [Header("Spawn range X")] [Tooltip("Se debe ingresar el rango en X de spawneo de los enemigos.")] public float randRange = 30f;
+   // [Header("Spawn range X")] [Tooltip("Se debe ingresar el rango en X de spawneo de los enemigos.")]
+    private float randRange = 50f;
     private int spawned;
     private float nexSpawn = 0.0f;
     // Start is called before the first frame update
