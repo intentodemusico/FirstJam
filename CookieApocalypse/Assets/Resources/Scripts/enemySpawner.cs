088dd936ad08d61fef05f29792087999856548d6
Oswalman <oswaldo-007@hotmail.com>
2019-06-17 21:02:11 -0500

    ascensor

---
+++ CookieApocalypse/Assets/Resources/Scripts/enemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+
+    private float randX;
+    
+    private Vector2 whereToSpawn;
+    [Header("Spawn rate")] [Tooltip("Se debe ingresar la velocidad de spawneo de los enemigos.")] public float spawnRate = 2f;
+    
+    [Header("Spawn range X")] [Tooltip("Se debe ingresar el rango en X de spawneo de los enemigos.")] public float randRange = 4f;
+
+    private float nexSpawn = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nexSpawn)
+        {
+            nexSpawn = Time.time + spawnRate;
+            randX = Random.Range(-randRange, randRange);
+            whereToSpawn=new Vector2(randX,transform.position.y);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+}
