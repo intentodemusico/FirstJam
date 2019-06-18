e3497b723aae0bed336cdd19a59a99367a7b9db4
Oswalman <oswaldo-007@hotmail.com>
2019-06-18 11:39:20 -0500

    Correcion segundo nivel

---
+++ CookieApocalypse/Assets/win.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win : MonoBehaviour
+{
+    private bool bandera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bandera = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bandera = true;
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        SceneManager.LoadScene("SegundoNivel");
+    }
+}
